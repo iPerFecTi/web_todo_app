d623cd68d4c2d453656f4ceac3e727c0e0a6a5c2
Bob <Valentin-Rager@t-online.de>
2025-10-23 20:17:53 +0200

    add requirements

--- Web.py
+++ Web.py
@@ -12,7 +12,8 @@ todos = Functions.get_todos()
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity.")
+st.write("This app is to increase your <b>productivity</b>.",
+         unsafe_allow_html=True)
 
 
 for index, todo in enumerate(todos):
