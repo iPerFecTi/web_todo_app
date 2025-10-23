33fd4f6807619cadeb304ca60e838f918091d83c
Bob <Valentin-Rager@t-online.de>
2025-10-23 20:46:21 +0200

    add requirements

--- Web.py
+++ Web.py
@@ -3,6 +3,8 @@ import Functions
 
 todos = Functions.get_todos()
 
+st.set_page_config(layout="wide")
+
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
@@ -12,7 +14,7 @@ todos = Functions.get_todos()
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your <b>productivity</b>.",
+st.write("<h1>This app is to increase your <b>productivity</b>.</h1>",
          unsafe_allow_html=True)
 
 
